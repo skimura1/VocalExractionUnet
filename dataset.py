4c709a1c0401905093471269f87c565aa3735e55
Skyler Kimura <skyler8@hawaii.edu>
2024-12-19 00:09:42 -1000

    Added audio augmentations for training set

--- dataset.py
+++ dataset.py
@@ -1,10 +1,16 @@
 import random
+from cmath import acosh
 
 import torch
 from torch.utils.data import Dataset
 
 import musdb
 
+def apply_augmentations(audio, gain):
+    audio = audio * gain
+    if random.random() > 0.5:
+        audio = audio.flip(0)
+    return audio
 
 class MUSDBDataset(Dataset):
     def __init__(
@@ -15,7 +21,7 @@ class MUSDBDataset(Dataset):
         subsets = "train",
         split = "train",
         seq_duration = 6.0,
-        samples_per_track = 64
+        samples_per_track = 64,
     ):
         self.is_wav = is_wav
         self.subsets = subsets
@@ -36,38 +42,37 @@ class MUSDBDataset(Dataset):
 
     def __getitem__(self, index):
         audio_sources = []
-        target_idx = None
-        track_idx = index // self.samples_per_track
-        
-        # select track
-        track = self.mus[index // self.samples_per_track]
-        if self.split == "train" and self.seq_duration:
-            for k, source in enumerate(self.mus.setup["sources"]):
-                # select a random track
-                track = random.choice(self.mus.tracks)
+        # random augment gain
+        random_gain = 0.25 + torch.rand(1) * (1.25 - 0.25)
 
-                # set the excerpt duration
-                track.chunk_duration = self.seq_duration
-                
-                # set random start position
-                track.chunk_start = random.uniform(0, track.duration - self.seq_duration)
-                
-                # load source audio and apply time domain source_augmentations
-                audio = torch.as_tensor(track.sources[source].audio.T, dtype=torch.float32)
-                # random augment gain
-                random_gain = 0.25 + torch.rand(1) * (1.25 - 0.25)
-                gain_audio = audio * random_gain
-                # channel swap
-                if random.random() > 0.5:
-                    channel_audio = gain_audio.flip(0)
-                audio_sources.append(audio)
+        # select track
+        target_track = self.mus[index // self.samples_per_track]
+        if self.split == "train" and self.seq_duration:
+            for source in self.mus.setup["sources"]:
+                if source != "vocals":
+                    # select a random track
+                    accompaniment_track = random.choice(self.mus.tracks)
+                    # set the excerpt duration
+                    accompaniment_track.chunk_duration = self.seq_duration
+                    # set random start position
+                    accompaniment_track.chunk_start = random.uniform(0, target_track.duration - self.seq_duration)
+
+                    # load source audio and apply time domain source_augmentations
+                    source_audio = torch.as_tensor(accompaniment_track.sources[source].audio.T, dtype=torch.float32)
+
+                    # random augment gain
+                    source_audio = apply_augmentations(source_audio, random_gain)
+                    audio_sources.append(source_audio)
+
+            target_track.chunk_duration = self.seq_duration
+            target_track.chunk_start = random.uniform(0, target_track.duration - self.seq_duration)
+            vocal = torch.as_tensor(target_track.targets["vocals"].audio.T, dtype=torch.float32)
+            audio_sources.append(vocal)
             stems = torch.stack(audio_sources, dim=0)
             mix = stems.sum(dim=0)
-            vocal_idx = list(self.mus.setup["sources"]).index("vocals")
-            vocal = mix - stems[vocal_idx]
         else:
-            mix = torch.as_tensor(track.audio.T, dtype=torch.float32)
-            vocal = torch.as_tensor(track.targets["vocals"].audio.T, dtype=torch.float32)
+            mix = torch.as_tensor(target_track.audio.T, dtype=torch.float32)
+            vocal = torch.as_tensor(target_track.targets["vocals"].audio.T, dtype=torch.float32)
 
         return mix, vocal
 
